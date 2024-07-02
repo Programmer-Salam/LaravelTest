c3d40b37744aba8e32fbaaf11279e43b95f4c0f2
BADMUS-IRETOMIWA-ABDULSALAM <96327283+Programmer-Salam@users.noreply.github.com>
2024-07-02 18:03:49 +0100

    Add files via upload

--- laravelfastructuretest.sql
+++ laravelfastructuretest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 01, 2024 at 07:24 PM
+-- Generation Time: Jul 02, 2024 at 07:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,6 +38,43 @@ CREATE TABLE `affiliates` (
   `affiliate_note` text DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `affiliate_comissions`
+--
+
+CREATE TABLE `affiliate_comissions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `type_name` varchar(255) NOT NULL,
+  `minimum_rate` int(11) NOT NULL,
+  `maximum_rate` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `deposit_rule` varchar(255) NOT NULL,
+  `deposit_included_methods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`deposit_included_methods`)),
+  `withdraw_rule` varchar(255) NOT NULL,
+  `withdraw_included_methods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`withdraw_included_methods`)),
+  `deduction_rule` varchar(255) NOT NULL,
+  `deduction_included_methods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`deduction_included_methods`)),
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `affiliate_network_types`
+--
+
+CREATE TABLE `affiliate_network_types` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `network` varchar(255) NOT NULL,
+  `network_domain_type` varchar(255) NOT NULL,
+  `network_domain` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -135,7 +172,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2024_07_01_114139_create_affiliates_table', 2);
+(4, '2024_07_01_114139_create_affiliates_table', 2),
+(7, '2024_07_02_125545_create_affiliate_groups_table', 3),
+(8, '2024_07_02_132557_create_affiliate_network_types_table', 3),
+(9, '2024_07_02_141338_create_affiliate_comissions_table', 4);
 
 -- --------------------------------------------------------
 
@@ -169,9 +209,9 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('58DtOYMgVEo7E4XV3A2iGDb3FCm28iMFUtjLup6Z', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiR2Y5dWwzVXRPMFJ6cE1pSVF4elR2ckFwMHZmbTRTQnhCR2JGRW9HWCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1719848681),
-('vL4gpIZixnvkk88tShhd4DXYUUmXMGTK6ykl82xn', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZDlWckVNMGlNTUd3WGZUenVuV0VTcFJja3BpN2M1NmdCNVF0ZEZYaiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1719853950),
-('vYwxt8f5PghgQnGkL1qVciZl8MtqeFl8tLybqUhO', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoid1JpdnNWcmg1QmdGazdBT3l0QW10RHNtNGQza1VkMlVkTVE5dHVEUiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1719854306);
+('2wwzRHzHNcZPNhqqZ9XY88wAa1GQ85GNuQpNMRLW', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoib2JSblE4bEEwajdIQm9xbTJEMW9kRjZuM0VmUERmTWdSZWNkZXlDdSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjE6e2k6MDtzOjE1OiJzdWNjZXNzX25ldHdvcmsiO31zOjM6Im5ldyI7YTowOnt9fX0=', 1719938754),
+('dqpzoiFx37uIYTws9AGt3hGyU5C12OyWqxJTU8Fo', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoieXo4Y0pUaG5tdDI4TGxuZWM4SE0yVlFBekRYQU1mQlhBYmpHWXRkbyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzg6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9hZmZpbGlhdGUtZ3JvdXBzIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1719939262),
+('rBZdKR05firVcP3fpYqsVKzgk7P49DXL9QuRd6ff', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiakcwNkt1OWwyOU02NmZySUdFZDlOemtxZ2VlYnd4Z2VtZWZ6ODNCbCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzg6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9hZmZpbGlhdGUtZ3JvdXBzIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1719931435);
 
 -- --------------------------------------------------------
 
@@ -232,6 +272,18 @@ INSERT INTO `users` (`id`, `user_id`, `user_tag`, `merchant_id`, `sub_merchant_i
 -- Indexes for table `affiliates`
 --
 ALTER TABLE `affiliates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `affiliate_comissions`
+--
+ALTER TABLE `affiliate_comissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `affiliate_network_types`
+--
+ALTER TABLE `affiliate_network_types`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -306,6 +358,18 @@ ALTER TABLE `affiliates`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `affiliate_comissions`
+--
+ALTER TABLE `affiliate_comissions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `affiliate_network_types`
+--
+ALTER TABLE `affiliate_network_types`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -321,7 +385,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
